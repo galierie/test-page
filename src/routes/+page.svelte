98096f22f755653441575b662027cb6d53f925ef
Eriene Galinato <edgalinato@up.edu.ph>
2025-11-09 00:56:32 +0800

    test: try different ways for `asset`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	import { asset } from '$app/paths';
-	import unresolvedBackground from '$lib/assets/flood.jpg';
-	import unresolvedNutshellBudget from '$lib/assets/graphs/nutshell_budget.jpg';
 
-	const background = asset(unresolvedBackground);
-	const nutshellBudget = asset(unresolvedNutshellBudget);
+	import nutshellBudget from '$lib/assets/graphs/nutshell_budget.jpg';
+
+	const background = asset('$lib/assets/flood.jpg');
 </script>
 
 <img src={background} alt="" class="h-auto w-full" />
-<img src={nutshellBudget} alt="" class="h-auto w-full" />
+<img src={asset(nutshellBudget)} alt="" class="h-auto w-full" />
