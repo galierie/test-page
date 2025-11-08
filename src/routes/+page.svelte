a79ae0a162adaf5b19c82824db439776380b5cfb
Eriene Galinato <edgalinato@up.edu.ph>
2025-11-08 23:04:08 +0800

    test: show `paths.base`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
-<script>
+<script lang="ts">
+	import { base } from '$app/paths';
 	import background from '$lib/assets/flood.jpg';
 	import nutshellBudget from '$lib/assets/graphs/nutshell_budget.jpg';
 </script>
 
-<h1>Images</h1>
+<p>{base}</p>
 <img src={background} alt="" class="h-auto w-full" />
 <img src={nutshellBudget} alt="" class="h-auto w-full" />
