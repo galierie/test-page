dc6e327a537301ba9709a5aa193ecf7b01fae019
Eriene Galinato <edgalinato@up.edu.ph>
2025-11-08 21:02:50 +0800

    feat: add test svelte app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import background from '$lib/assets/flood.jpg';
+	import nutshellBudget from '$lib/assets/graphs/nutshell_budget.jpg';
+</script>
+
+<h1>Images</h1>
+<img src={background} alt="" />
+<img src={nutshellBudget} alt="" />
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit(), enhancedImages()]
+});
