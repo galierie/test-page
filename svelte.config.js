43af35141d3707ffb3c00daab2417ae437b23252
Eriene Galinato <edgalinato@up.edu.ph>
2025-11-08 22:29:56 +0800

    fix(`svelte.config`): prefer "production" keyword

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
-		paths: { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
+		paths: { base: process.argv.includes('production') ? process.env.BASE_PATH : '' }
 	}
 };
 
